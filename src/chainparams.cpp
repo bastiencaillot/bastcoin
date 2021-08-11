163b25ca5542fdb4b72d9be9c45caf04cb3dcc78
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-11 21:56:28 +0200

    change timestamp because genesis block has not been mined yesterday

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "CNBC Aug 10 2021 As Bitcoin looks to go ‘green,’ some miners look to nuclear power as a possible solution";
+    const char* pszTimestamp = "CNBC Aug 11 2021 Amazon opens its $1.5 billion air hub in Kentucky in latest push to speed deliveries";
     const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
