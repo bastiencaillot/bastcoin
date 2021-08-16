39235d0499ac7c99cf9528d4c399e8fee50c38e1
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-16 18:05:48 +0200

    #if defined(__SSE4_1__)

--- src/crypto/blake3_sse41.c
+++ src/crypto/blake3_sse41.c
@@ -1,5 +1,7 @@
 #include "blake3_impl.h"
 
+#if defined(__SSE4_1__)
+
 #include <immintrin.h>
 
 #define DEGREE 4
@@ -557,3 +559,5 @@ void blake3_hash_many_sse41(const uint8_t *const *inputs, size_t num_inputs,
     out = &out[BLAKE3_OUT_LEN];
   }
 }
+
+#endif
