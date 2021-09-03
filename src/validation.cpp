c0cf31618d3269119b9dc29bf24a0d573e236b99
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-09-03 18:24:14 +0200

    bool fEnforceBIP30 = true;

--- src/validation.cpp
+++ src/validation.cpp
@@ -1870,6 +1870,7 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
     // Now that the whole chain is irreversibly beyond that time it is applied to all blocks except the
     // two in the chain that violate it. This prevents exploiting the issue against nodes during their
     // initial block download.
+    bool fEnforceBIP30 = true;
     //bool fEnforceBIP30 = !((pindex->nHeight==91842 && pindex->GetBlockHash() == uint256S("0x00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec")) ||
                           //(pindex->nHeight==91880 && pindex->GetBlockHash() == uint256S("0x00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721")));
 
