cad0979c29476c87805ff3e347b9413059fbc585
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-09-27 21:40:51 +0200

    add SetThreadPriority

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -1220,6 +1220,19 @@ int64_t GetStartupTime()
     return nStartupTime;
 }
 
+void SetThreadPriority(int nPriority)
+{
+#ifdef WIN32
+    SetThreadPriority(GetCurrentThread(), nPriority);
+#else // WIN32
+#ifdef PRIO_THREAD
+    setpriority(PRIO_THREAD, 0, nPriority);
+#else // PRIO_THREAD
+    setpriority(PRIO_PROCESS, 0, nPriority);
+#endif // PRIO_THREAD
+#endif // WIN32
+}
+
 fs::path AbsPathForConfigVal(const fs::path& path, bool net_specific)
 {
     return fs::absolute(path, GetDataDir(net_specific));
